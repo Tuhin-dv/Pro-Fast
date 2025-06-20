f1c2b7c9a4d0f86d320efbc7725a02fac4a5ef85
Tuhin-dev <tawhiduli682@gmail.com>
2025-06-20 21:17:11 +0600

    client side . create some component and pages and navbar added

---
+++ zap-shift-client/src/routes/router.jsx
@@ -0,0 +1,18 @@
+
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../root/MainLayout";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+ {
+    path: '/',
+    Component: MainLayout,
+    children: [
+        {
+          index: true,
+          Component: Home
+        }
+    ]
+ }
+   
+]);
